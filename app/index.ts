fc999c3049ace6c668a7ecf14c247a86aa27feac
tiana <tiana.randrianarivony@gmail.com>
2024-10-04 13:50:55 +0300

    editor won't up

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,14 @@
 import Drawflow from 'drawflow'
-
-import  'drawflow/dist/drawflow.min.css'
-
+// import styleDrawflow from 'drawflow/dist/drawflow.min.css'
 
 window.addEventListener("load", (event) => {
     const id: HTMLElement = document.getElementById("drawflow") as HTMLElement;
-    const editor = new Drawflow(id);
-    editor.start();
+    const editor: Drawflow = new Drawflow(id);
+    var html = `
+<div><input type="text" df-name></div>
+`;
+    var data = {"name": ''};
+
+    editor.addNode('github', 1, 1, 150, 300, 'github', data, html, false);
 });
 
