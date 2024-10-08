fe6444a6e0a04a01b9e33936821744a7f289f5d3
tiana <tiana.randrianarivony@gmail.com>
2024-10-08 10:22:14 +0300

    set add condition item icon

--- src/static_data/conditions.ts
+++ src/static_data/conditions.ts
@@ -6,7 +6,7 @@ window.remove_item_row = (event: MouseEvent) => {
 
 window.add_condition_row = (e: Event) => {
     const container = e.target?.parentElement.parentElement;
-    container.innerHTML += `<div class="flex gap-3 mt-2">${row}<img  onclick="remove_item_row(event)" src="/icons/trash.svg" class="w-4 hover:cursor-pointer" alt="trash"/> </div>`
+    container.innerHTML += `<div class="flex gap-3 mt-2">${row}<img  onclick="remove_item_row(event)" src="/icons/trash.svg" class="w-5 ml-0.5 hover:cursor-pointer" alt="trash"/> </div>`
 }
 
 
@@ -14,7 +14,8 @@ const get_conditions_template = () => `
 <div class="container flex flex-col gap-2">
     <div class="rows">
         <div class="flex gap-3">    
-            ${row}  <button class="btn" onclick="add_condition_row(event)">+</button>
+            ${row} 
+             <img  onclick="add_condition_row(event)" src="/icons/add.svg" class="w-6 hover:cursor-pointer" alt="trash"/> 
         </div>
     </div>
 
