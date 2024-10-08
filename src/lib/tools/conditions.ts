31e82a06f8c061f16cb953c72c8bf357e2c2f016
tiana <tiana.randrianarivony@gmail.com>
2024-10-08 09:21:37 +0300

    add multiple condition row

---
+++ src/lib/tools/conditions.ts
@@ -0,0 +1,20 @@
+const fields = Array.from({length: 10}, (_v, i) => 'field ' + (i + 1))
+const values = Array.from({length: 10}, (_v, i) => 'value ' + (1 + i))
+const operators = [
+    '==', '!=', '>', '<', '>=', '<='
+
+];
+const createButton = (): HTMLButtonElement => document.createElement('button')
+const row = `    
+        <select class="select" name="condition">
+            ${fields.map(field => `<option value="${field}">${field}</option>`).join('')}
+        </select>
+        <select class="select" name="operator_value">
+            ${operators.map(field => `<option value="${field}">${field}</option>`).join('')}
+        </select>
+        <select class="select" name="condition_value">
+            ${values.map(field => `<option value="${field}">${field}</option>`).join('')}
+        </select>
+    `
+
+export { row, createButton}
--- src/static_data/conditions.ts
+++ src/static_data/conditions.ts
@@ -1,37 +1,24 @@
-const fields = Array.from({length: 10}, (_v, i) => 'field ' + (i + 1))
-const values = Array.from({length: 10}, (_v, i) => 'value ' + (1 + i))
+import {createButton, row} from "@/lib/tools/conditions.ts";
 
-
-const operators = [
-    '==', '!=', '>', '<', '>=', '<='
-
-];
 window.add_condition_row = (e: Event) => {
-    const container = e.target?.parentElement.parentElement.firstElementChild;
-    container.innerHTML += row
+    const container = e.target?.parentElement.parentElement;
+    const deleteButton = createButton()
+    deleteButton.textContent = '-'
+    deleteButton.classList.add('btn','danger')
+    container.innerHTML += `<div class="flex gap-3 mt-2">${row}</div>`
+    container.lastChild.append(deleteButton)
 }
-const row = `    <div class="flex gap-3">
-        <select class="select" name="condition">
-            ${fields.map(field => `<option value="${field}">${field}</option>`).join('')}
-        </select>
-        <select class="select" name="operator_value">
-            ${operators.map(field => `<option value="${field}">${field}</option>`).join('')}
-        </select>
-        <select class="select" name="condition_value">
-            ${values.map(field => `<option value="${field}">${field}</option>`).join('')}
-        </select>
-    </div>
-    `
+
+
 
 const get_conditions_template = () => `
 <div class="container flex flex-col gap-2">
-<div class="rows">
-    ${row}
-</div>
-
-    <div class="my-auto mx-2">
-        <button class="btn" onclick="add_condition_row(event)">+</button>
+    <div class="rows">
+        <div class="flex gap-3">    
+            ${row}  <button class="btn" onclick="add_condition_row(event)">+</button>
+        </div>
     </div>
+
 </div>
 
 `
