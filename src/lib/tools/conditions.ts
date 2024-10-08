4c163796351f0d307f21491ec080647e42267fc1
tiana <tiana.randrianarivony@gmail.com>
2024-10-08 14:08:32 +0300

    review set add row button

--- src/lib/tools/conditions.ts
+++ src/lib/tools/conditions.ts
@@ -1,27 +1,33 @@
 import {conditions_candidates, ConditionType} from "@/static_data/conditions.ts";
+
+enum ButtonAction {
+    ADD = 'ADD',
+    DELETE = 'DELETE'
+}
+
 const remove_item_button = () => `<img  onclick="remove_condition_item_row(event)" src="/icons/trash.svg" class="w-5 ml-0.5 hover:cursor-pointer" alt="trash"/> `
-const add_item_button = () => `<img  onclick="add_condition_row(event)" src="/icons/add.svg" class="w-6 hover:cursor-pointer" alt="trash"/> `
+const add_item_button = () => `<img  onclick="add_condition_row(event)" src="/icons/add.svg" class="w-6 hover:cursor-pointer" alt="plus"/> `
 window.handleFieldChange = (e: Event) => {
     const target = e.target as HTMLSelectElement;
     const selectedValue = target.value.trim();
     const source = conditions_candidates.find(c => c.field.trim() === selectedValue);
 
     if (source) {
-        updateRow(source, target.parentElement);
+        updateRow(source, target.parentElement, ButtonAction.DELETE);
     }
 };
 window.remove_condition_item_row = (event: MouseEvent) => {
     event.target?.parentElement.remove()
 }
 
-
 window.add_condition_row = (e: Event) => {
     const container = e.target?.parentElement.parentElement;
-    container.innerHTML += `<div class="flex gap-3 mt-2">${createRow() + remove_item_button()}</div>`
+    container.innerHTML += `<div class="flex gap-3 mt-2">${createRow()}</div>`
 }
 
-const updateRow = (source: ConditionType, container: HTMLElement|null = null) => {
-    const rowContent = `
+const updateRow = (source: ConditionType, rowContainer: HTMLElement | null = null, action: ButtonAction = ButtonAction.ADD) => {
+    const isFirstRow = rowContainer?.parentElement?.childElementCount === 1
+    let rowContent = `
         <select class="select" name="condition" onchange="handleFieldChange(event)">   
             ${conditions_candidates.map(candidate => `<option ${candidate.field === source.field ? 'selected' : ''}>${candidate.field}</option>`).join('')}
         </select>
@@ -33,10 +39,11 @@ const updateRow = (source: ConditionType, container: HTMLElement|null = null) =>
                 ${source.values.map(value => `<option value="${value}">${value || '(null)'}</option>`).join('')}
             </select>` :
         `<input type="text" name="condition_value" placeholder="(null)" />`}
+        
     `;
-
-    if(!container) return rowContent
-    container.innerHTML = rowContent;
+    rowContent += isFirstRow || action == ButtonAction.ADD ? add_item_button() : remove_item_button()
+    if (!rowContainer) return rowContent
+    rowContainer.innerHTML = rowContent;
 };
 
 const createRow = () => {
@@ -44,4 +51,4 @@ const createRow = () => {
     return updateRow(defaultSource);
 };
 
-export { createRow, add_item_button };
+export {createRow};
