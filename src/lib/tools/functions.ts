18502f000fbf86438bdcb6bb930cf10d4806dad0
tiana <tiana.randrianarivony@gmail.com>
2024-10-09 17:01:42 +0300

    notify user when can not able to set both action and condition in the same lead

--- src/lib/tools/functions.ts
+++ src/lib/tools/functions.ts
@@ -1,8 +1,11 @@
-const generateUUID = () => 'xxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = Math.random() * 16 | 0;
-        const v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+
+const generateUUID = () => 'xxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+});
 
 
 const extractFormData = (form: HTMLFormElement): Record<string, Record<string, string>> => {
@@ -13,7 +16,7 @@ const extractFormData = (form: HTMLFormElement): Record<string, Record<string, s
             if (element.name) {
                 const match = element.name.match(/^(?<name>[^[]+)\[(?<uuid>.+)\]$/);
                 if (match && match.groups) {
-                    const { name, uuid } = match.groups;
+                    const {name, uuid} = match.groups;
 
                     // Crée l'objet pour l'UUID s'il n'existe pas
                     if (!values[uuid]) {
@@ -26,8 +29,21 @@ const extractFormData = (form: HTMLFormElement): Record<string, Record<string, s
             }
         }
     }
-console.log(values)
+    console.log(values)
     return values;
 };
 
-export {generateUUID, extractFormData}
+const notify = (message: string, duration: number = 2000, class_name: string | null = null) => {
+    Toastify({
+        text: message,
+        duration,
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        className: class_name || ''
+    }).showToast();
+}
+
+export {generateUUID, extractFormData, notify}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import Drawflow from 'drawflow'
+import Drawflow, {ConnectionEvent} from 'drawflow'
 import '@/styles/base.css'
 import 'drawflow/dist/drawflow.min.css'
 import '@/styles/workflow.scss'
 import {handleDrag} from "@/lib/node.ts";
 import statuses from "@/static_data/statuses.ts";
+import {NodeType} from "@/lib/constant.ts";
+import {notify} from "@/lib/tools/functions.ts";
+
+
 let selectionId: number = 0
 let editor: Drawflow;
 
@@ -13,6 +17,7 @@ window.addEventListener("load", () => {
     const id = document.getElementById("drawflow") as HTMLElement;
 
     editor = new Drawflow(id);
+
     editor.on('nodeCreated', (e) => {
         const el = editor.getNodeFromId(e)
         if (el.data.type === 'status') {
@@ -38,10 +43,25 @@ window.addEventListener("load", () => {
     editor.on('nodeSelected', (e) => {
         selectionId = e
     })
+    editor.on('connectionCreated', (event: ConnectionEvent) => {
+        const {input_id, output_id} = event
+        const from_payload = editor.getNodeFromId(output_id)
+        const to_payload = editor.getNodeFromId(input_id)
 
-    editor.on('connectionCreated', (a) => {
-        console.log(a)
-        // return true
+        const {name: from_name, id: from_id} = from_payload
+        const {name: to_name, id: to_id} = to_payload
+
+        if ([NodeType.status, NodeType.action].includes(<NodeType>from_name) && to_name === NodeType.conditions) {
+            to_payload.inputs.input_1.connections.forEach(({node}) => {
+                const _node = editor.getNodeFromId(node)
+                if (_node.name !== from_payload.name) {
+                    editor.removeSingleConnection(from_id, to_id, 'output_1', 'input_1')
+                    notify("Un status ne peut pas être lier à la fois à une condition et une action.", 2000, "notification alert")
+                    return
+                }
+            })
+        }
+
     })
 
     document.getElementById('export_action')?.addEventListener('click', () => {
